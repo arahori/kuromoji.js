bf8611c72d87895569cda3baebeedd4bb205bd13
y.arahori <yuuta.arahori@jp.ricoh.com>
2023-11-09 08:21:15 +0900

    node_modules を読み込ませられるか確認

--- src/TokenizerBuilder.js
+++ src/TokenizerBuilder.js
@@ -15,10 +15,10 @@
  * limitations under the License.
  */
 
-"use strict";
+'use strict';
 
-var Tokenizer = require("./Tokenizer");
-var DictionaryLoader = require("./loader/NodeDictionaryLoader");
+var Tokenizer = require('./Tokenizer');
+var DictionaryLoader = require('./loader/BrowserDictionaryLoader');
 
 /**
  * TokenizerBuilder create Tokenizer instance.
@@ -27,11 +27,11 @@ var DictionaryLoader = require("./loader/NodeDictionaryLoader");
  * @constructor
  */
 function TokenizerBuilder(option) {
-    if (option.dicPath == null) {
-        this.dic_path = "dict/";
-    } else {
-        this.dic_path = option.dicPath;
-    }
+  if (option.dicPath == null) {
+    this.dic_path = 'dict/';
+  } else {
+    this.dic_path = option.dicPath;
+  }
 }
 
 /**
@@ -39,10 +39,10 @@ function TokenizerBuilder(option) {
  * @param {TokenizerBuilder~onLoad} callback Callback function
  */
 TokenizerBuilder.prototype.build = function (callback) {
-    var loader = new DictionaryLoader(this.dic_path);
-    loader.load(function (err, dic) {
-        callback(err, new Tokenizer(dic));
-    });
+  var loader = new DictionaryLoader(this.dic_path);
+  loader.load(function (err, dic) {
+    callback(err, new Tokenizer(dic));
+  });
 };
 
 /**
